75e0c76da918b9e0a0c5e969ceccb2b07e0061c7
spal7580 <psaineeeraj@gmail.com>
2016-10-28 09:08:47 -0600

    memes list added and political list

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -2,12 +2,6 @@ package chat.model;
 
 import java.util.ArrayList;
 
-/**
- * Base version of the 2016 Chatbot class. Only stub methods are provided. Students will complete methods as part
- * of the project.
- * @author Sai Paladugu
- * @version 1.0 10/14/16
- */
 public class Chatbot
 {
 	private ArrayList<String> memesList;
@@ -15,10 +9,6 @@ public class Chatbot
 	private String userName;
 	private String content;
 	
-	/**
-	 * Creates an instance of the Chatbot with the supplied username.
-	 * @param userName The username for the chatbot.
-	 */
 	public Chatbot(String userName)
 	{
 		this.memesList = new ArrayList<String>();
@@ -29,20 +19,47 @@ public class Chatbot
 	
 	private void buildMemesList()
 	{
-		
+		memesList.add("doge");
+		memesList.add("cute animals");
+		memesList.add("grumpy cat");
+		memesList.add("dat boi");
+		memesList.add("willy wonka");
+		memesList.add("harambe");
+		memesList.add("john cena");
+		memesList.add("pepe");
+		memesList.add("pupper");
+		memesList.add("doot doot");
+		memesList.add("bad luck brian");
+		memesList.add("one does not simply");
+		memesList.add("futurama fry");
+		memesList.add("y u no");
+		memesList.add("science man");
+		memesList.add("not bad obama");
+		memesList.add("pepe boi");
 	}
 	
 	private void buildPoliticalTopicsList()
 	{
-		
+		politicalTopicList.add("Democrat");
+		politicalTopicList.add("Republican");
+		politicalTopicList.add("conservative");
+		politicalTopicList.add("Clinton");
+		politicalTopicList.add("Trump");
+		politicalTopicList.add("Kaine");
+		politicalTopicList.add("Pence");
+		politicalTopicList.add("Stein");
+		politicalTopicList.add("ohnson");
+		politicalTopicList.add("election");
+		politicalTopicList.add("Vote");
+		politicalTopicList.add("Obama");
+		politicalTopicList.add("Immigration");
+		politicalTopicList.add("White House");
+		politicalTopicList.add("Dedt");
+		politicalTopicList.add("Women rights");
+		politicalTopicList.add("muslim discrimination");
 	}
 	
-	/**
-	 * Checks the length of the supplied string. Returns false if the supplied String is empty or null,
-	 * otherwise returns true. 
-	 * @param currentInput
-	 * @return A true or false based on the length of the supplied String.
-	 */
+
 	public boolean lengthChecker(String currentInput)
 	{
 		boolean hasLength = false;
@@ -53,78 +70,56 @@ public class Chatbot
 		return hasLength;
 	}
 	
-	/**
-	 * Checks if the supplied String matches the content area for this Chatbot instance.
-	 * @param currentInput The supplied String to be checked.
-	 * @return Whether it matches the content area.
-	 */
+
 	public boolean contentChecker(String currentInput)
 	{
-		return false;
+		boolean containsinput = false;
+		if (currentInput.contains("feet"))
+		{
+			return containsinput = true;
+		}
+		return containsinput;
 	}
 	
-	/**
-	 * Checks if supplied String matches ANY of the topics in the politicalTopicsList. Returns
-	 * true if it does find a match and false if it does not match.
-	 * @param currentInput The supplied String to be checked.
-	 * @return Whether the String is contained in the ArrayList.
-	 */
+	
 	public boolean politicalTopicChecker(String currentInput)
 	{
 		return false;
 	}
 	
 	
-	/**
-	 * Checks to see that the supplied String value is in the current memesList variable.
-	 * @param currentInput The supplied String to be checked.
-	 * @return Whether the supplied String is a recognized meme.
-	 */
 	public boolean memeChecker(String currentInput)
 	{
-		return false;
+		boolean hasinput = false;
+		if (currentInput != null && memesList.contains(currentInput)){hasinput = true;}
+		{
+			return hasinput = true;
+		}
 	}
 	
-	/**
-	 * Returns the username of this Chatbot instance.
-	 * @return The username of the Chatbot.
-	 */
+	
 	public String getUserName()
 	{
 		return userName;
 	}
 	
-	/**
-	 * Returns the content area for this Chatbot instance.
-	 * @return The content area for this Chatbot instance.
-	 */
+	
 	public String getContent()
 	{
 		return content;
 	}
 	
-	/**
-	 * Getter method for the memesList object.
-	 * @return The reference to the meme list.
-	 */
 	public ArrayList<String> getMemesList()
 	{
+		
 		return memesList;
 	}
 	
-	/**
-	 * Getter method for the politicalTopicList object.
-	 * @return The reference to the political topic list.
-	 */
 	public ArrayList<String> getPoliticalTopicList()
 	{
 		return politicalTopicList;
 	}
 	
-	/**
-	 * Updates the content area for this Chatbot instance.
-	 * @param content The updated value for the content area.
-	 */
 	public void setContent(String content)
 	{
 		this.content = content;
